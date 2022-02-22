9567ba33bc29c564fcc7114cec191815de27b09d
BregtjedeMes <bregtjedemes@gmail.com>
2022-02-22 12:18:55 +0100

    art studios aanpassing Mark Ros

--- embed/markros/custom.js
+++ embed/markros/custom.js
@@ -12,12 +12,12 @@ function renderHtml() {
         <section class="my-section">
             <div class="frame-content-container">
                 <div class="img-container-1 justify-content-center">
-                    <img src="https://thekitchenarchitects.nl/wp-content/uploads/2020/01/kitchen-arcjitects-logo.png" />
+                    <img src="https://thekitchenartstudios.nl/wp-content/uploads/2020/01/kitchen-arcjitects-logo.png" />
                 </div>
                 <div class="text-container-1">
-                    <h2>Welkom bij ‘The Kitchen Architects’!</h2>
+                    <h2>Welkom bij ‘The Kitchen Art Studios’!</h2>
                     <p>
-                    Wij zijn aangesloten bij het samenwerkingsconcept ‘The Kitchen Architects’: een landelijk platform van ware keuken architecten. Onze verbindende factor is de passie voor het creëren van een unieke keuken als ware eyecatcher. Wij zijn zelfstandige ondernemers die de filosofie ‘We imagine your style’ kunnen vertalen vanuit uw wensen. Door de krachten landelijk te bundelen op het gebied van inkoop en marketing, kunnen wij u de hoogste kwaliteit en de beste leveranciers bieden.
+                    Wij zijn aangesloten bij het samenwerkingsconcept ‘The Kitchen Art Studios’: een landelijk platform van ware keuken art. Onze verbindende factor is de passie voor het creëren van een unieke keuken als ware eyecatcher. Wij zijn zelfstandige ondernemers die de filosofie ‘We imagine your style’ kunnen vertalen vanuit uw wensen. Door de krachten landelijk te bundelen op het gebied van inkoop en marketing, kunnen wij u de hoogste kwaliteit en de beste leveranciers bieden.
                     </p>
                 </div>
             </div>
@@ -27,7 +27,7 @@ function renderHtml() {
         <section class="row text-img">
             <div class="img-container col col-md-6">
                 <div>
-                    <img class="mvdr" src="https://thekitchenarchitects.nl/wp-content/uploads/2020/03/The-kitchen-architects-monique-interview-keukens-monique-van-der-reijden-DEF.jpeg" />
+                    <img class="mvdr" src="https://thekitchenartstudios.nl/wp-content/uploads/2020/03/The-kitchen-art-studios-monique-interview-keukens-monique-van-der-reijden-DEF.jpeg" />
                 </div>
                     </div>
             <div class="col col-md-6">
@@ -36,7 +36,7 @@ function renderHtml() {
                 <p>
                     Trendwatcher en forecaster bij o.a. Elle Decoration, Eigen
                     Huis & Interieur en de Telegraaf heeft speciaal voor The
-                    Kitchen Architects de 4 trends van de komende jaren samengesteld.
+                    Kitchen Art Studios de 4 trends van de komende jaren samengesteld.
                 </p>
                 <p>
                     Laat u inspireren door de stijlvolle combinaties van de trend Sereen Steen of durf te experimenteren met kleur en structuur
@@ -44,7 +44,7 @@ function renderHtml() {
                 </p>
                 <br>
                 <br>
-                <a href="https://thekitchenarchitects.nl/trends/" target="_blank">Bekijk onze trends</a>
+                <a href="https://thekitchenartstudios.nl/trends/" target="_blank">Bekijk onze trends</a>
             </div>
         </section>
         <br>
@@ -53,16 +53,16 @@ function renderHtml() {
             <div class="col col-md-6">
                 <h2>samenwerking <span class="line">'van woonvilla </span><span>naar droomvilla'</span class"line"> <span class="line">op rtl4</span></h2>
                 <p>
-                Wij, van The Kitchen Architects, hebben een uitstekende samenwerking met Designa Interieur en Architectuur: bekend van het RTL4 programma ‘Van Woonvilla naar Droomvilla’. In samenwerking met Designa worden de nieuwste trends gespot door ons op het gebied van statement keukens en innovatieve apparatuur.
+                Wij, van The Kitchen Art Studios, hebben een uitstekende samenwerking met Designa Interieur en Architectuur: bekend van het RTL4 programma ‘Van Woonvilla naar Droomvilla’. In samenwerking met Designa worden de nieuwste trends gespot door ons op het gebied van statement keukens en innovatieve apparatuur.
                 </p>
                 <br>
-                <a href="https://thekitchenarchitects.nl/samenwerkingen/designa/" target="_blank">Lees hier meer</a>
+                <a href="https://thekitchenartstudios.nl/samenwerkingen/designa/" target="_blank">Lees hier meer</a>
             </div>
             <div class="img-container col col-md-6" id="rtl-section">
-                <img src="https://thekitchenarchitects.nl/wp-content/uploads/2020/05/Kitchen-Architects-samenwerkingen-designa-keukens-schilderij.png" />
-                <img class="img-logo" src="https://thekitchenarchitects.nl/wp-content/uploads/2021/02/Woonvilla-logo.png" />
+                <img src="https://thekitchenartstudios.nl/wp-content/uploads/2020/05/Kitchen-art-studios-samenwerkingen-designa-keukens-schilderij.png" />
+                <img class="img-logo" src="https://thekitchenartstudios.nl/wp-content/uploads/2021/02/Woonvilla-logo.png" />
                 <p id="tv">Bekend van TV</p>
-                <img class="img-logo" id="rtl4" src="https://thekitchenarchitects.nl/wp-content/uploads/2021/02/RTL4-logo.png" />
+                <img class="img-logo" id="rtl4" src="https://thekitchenartstudios.nl/wp-content/uploads/2021/02/RTL4-logo.png" />
             </div>
         </section>
         <br>  
@@ -70,7 +70,7 @@ function renderHtml() {
         <section class="row text-img">
             <div class="img-container col col-md-6">
                 <div>
-                    <img class="mvdr" src="https://thekitchenarchitects.nl/wp-content/uploads/2021/08/markross-blogs-embed-the-kitchen-architects.png" />
+                    <img class="mvdr" src="https://thekitchenartstudios.nl/wp-content/uploads/2021/08/markross-blogs-embed-the-kitchen-art-studios.png" />
                 </div>
                     </div>
             <div class="col col-md-6">
@@ -83,7 +83,7 @@ function renderHtml() {
                 <br>
                 <br>
                 <br>
-                <a href="https://thekitchenarchitects.nl/blogs/" target="_blank">Lees hier onze blogs</a>
+                <a href="https://thekitchenartstudios.nl/blogs/" target="_blank">Lees hier onze blogs</a>
             </div>
         </section>
         <br>        
@@ -102,7 +102,7 @@ function renderHtml() {
                         Maak een <a href="https://markroskeukenseninterieur.nl/" target="_blank">afspraak</a> voor een adviesgesprek en kom langs in onze inspiratieshowroom waar u de mooiste creaties ontdekt.
                     </p>
                     <p>
-                    In onze showroom treft u ons prachtige Stylebook met meer informatie over ons bedrijf en de nieuwste trends op het gebied van keukens & living. In dit Stylebook nemen wij u mee naar één van onze klanten, zodat u onze kwaliteit kunt ervaren. Laat u inspireren door The Kitchen Architects!
+                    In onze showroom treft u ons prachtige Stylebook met meer informatie over ons bedrijf en de nieuwste trends op het gebied van keukens & living. In dit Stylebook nemen wij u mee naar één van onze klanten, zodat u onze kwaliteit kunt ervaren. Laat u inspireren door The Kitchen Art Studios!
                     </p>
                 </div>
             </div>
@@ -115,7 +115,7 @@ function renderHtml() {
                 <br>
                 <br>
                 <br>
-                <p class="tka"><span>The</span> Kitchen Architects |</p>  
+                <p class="tka"><span>The</span> Kitchen Art Studios |</p>  
                 <p class="phrase">We Imagine Your Style</p>
             </div>
         </section>
